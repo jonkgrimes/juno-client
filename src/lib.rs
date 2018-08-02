cb132fdfb53d293a0c5981e8ab0ae6ec70d8875a
Jon Grimes <jong@spiceworks.com>
2018-08-02 14:00:29 -0500

    Use tokio runtime directly

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,49 @@
 extern crate rand;
+extern crate tokio;
+extern crate hyper;
 
 use std::thread;
 use std::time::Duration;
-use std::fmt;
+use hyper::{Client, Method, Request, Uri, Body};
+use hyper::rt::{self, Future, Stream};
+use tokio::runtime::Runtime;
 
 mod models;
 
 use models::MetricData;
 
 pub fn run(telemetry_url: &str) {
+  let mut runtime = Runtime::new().unwrap(); 
+  let uri: Uri = telemetry_url.parse().ok().expect("Couldn't parse telemetry URI");
+
   loop {
+    // Setup data
     let metric = MetricData::fake();
-    println!("{}", metric);
+
+    // Setup request
+    let client = Client::new();
+    let mut req = Request::new(Body::from(format!("{}", metric)));
+    *req.method_mut() = Method::POST;
+    *req.uri_mut() = uri.clone();
+
+    // Make request
+    let post = client.request(req).and_then(|res| {
+      println!("POST: {}", res.status());
+      res.into_body().concat2()
+    })
+    // If all good, just tell the user...
+    .map(|_| {
+        println!("Done.");
+    })
+    // If there was an error, let the user know...
+    .map_err(|err| {
+        eprintln!("Error {}", err);
+    });
+
+    runtime.spawn(post);
+
     thread::sleep(Duration::new(10, 0));
   }
+  
+  runtime.shutdown_on_idle().wait().unwrap(); 
 }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate juno_client;
 
 use juno_client::run;
 
-const DEFAULT_URL: &str = "http://localhost:8080";
+const DEFAULT_URL: &str = "http://localhost:8080/data";
 
 fn main() {
     run(DEFAULT_URL);
