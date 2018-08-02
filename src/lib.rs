4e2ecb56646415594ca55ea6882fb1c3e9da4ffd
Jon Grimes <jong@spiceworks.com>
2018-08-01 20:22:01 -0500

    Getting some stubs of data

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,33 @@
+extern crate futures;
+#[macro_use]
+extern crate hyper;
+
 use std::thread;
 use std::time::Duration;
+use std::fmt;
 
-pub fn run() {
+struct MetricData {
+  cpu: u32,
+  memory: u32,
+  network_in: u32,
+  network_out: u32,
+}
+
+impl fmt::Display for MetricData {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "CPU: {}%, Mem: {}%, Network In: {}%, Network Out: {}%", self.cpu, self.memory, self.network_in, self.network_out)
+  }
+}
+
+pub fn run(telemetry_url: &str) {
   loop {
-    println!("Hello World!");
-    thread::sleep(Duration::new(60, 0));
+    let metric = MetricData {
+      cpu: 100,
+      memory: 4096,
+      network_in: 2134123,
+      network_out: 1344592
+    };
+    println!("{}", metric);
+    thread::sleep(Duration::new(10, 0));
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate juno_client;
 
 use juno_client::run;
 
+const DEFAULT_URL: &str = "http://localhost:8080";
+
 fn main() {
-    run();
+    run(DEFAULT_URL);
 }
